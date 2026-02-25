62ad03234924c5d1bc1252deccc33f37e5ea90d3
Lecture-SHIN <lecture.yonggishin@gmail.com>
2026-02-25 16:25:32 +0900

    add step01-di

---
+++ step01-di/src/main/java/com/spring/di/controller/DIController.java
@@ -0,0 +1,14 @@
+package com.spring.di.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DIController {
+	
+	public DIController() {
+		System.out.println("DIController 생성자");
+	}
+	
+//	private DIService service = new DIService();
+	
+}
---
+++ step01-di/src/main/java/com/spring/di/service/DIService.java
@@ -0,0 +1,12 @@
+package com.spring.di.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DIService {
+	
+	public DIService() {
+		System.out.println("DIService 생성자");
+	}
+	
+}
