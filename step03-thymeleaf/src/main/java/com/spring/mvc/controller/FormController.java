ff25e3f82b9807d7e8aab4d7687b77305ef89079
Lecture-SHIN <lecture.yonggishin@gmail.com>
2026-02-27 11:11:15 +0900

    update step03-thymeleaf

--- step03-thymeleaf/src/main/java/com/spring/mvc/controller/FormController.java
+++ step03-thymeleaf/src/main/java/com/spring/mvc/controller/FormController.java
@@ -1,13 +1,24 @@
 package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.mvc.dto.Student;
+import com.spring.mvc.dto.Student.Grade;
 
 @Controller
 public class FormController {
 	
 	@GetMapping(value = "/form")
-	public String form() {
+	public String form(Student student,
+						Model model) {
+		System.out.println("FormController");
+		System.out.println(student);
+		student.setGrade(Grade.JUNIOR);
+		
+		model.addAttribute("student", student);
+		
 		return "test";
 	}
 	
