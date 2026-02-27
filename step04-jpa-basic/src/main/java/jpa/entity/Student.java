4eb9bd35e27128cab84fc05dfe00e39b6e21c8e7
Lecture-SHIN <lecture.yonggishin@gmail.com>
2026-02-27 14:28:47 +0900

    add step04-jpa-basic

---
+++ step04-jpa-basic/src/main/java/jpa/entity/Student.java
@@ -0,0 +1,17 @@
+package jpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+	private Integer sid;
+	private String sname;
+}
---
+++ step04-jpa-basic/src/main/java/jpa/test/PersistenceTest.java
@@ -0,0 +1,25 @@
+package jpa.test;
+
+public class PersistenceTest {
+	public static void main(String[] args) {
+		// EMF
+		
+		// EM
+		
+		// TX : begin() ~ 작업 수행 ~  commit()
+		
+		// insert
+		// 20241001, "DEV"
+		// 20241002, "DEVOPS"
+		
+		// select
+		// sid로 검색 20241001
+		
+		// update 
+		// DEVOPS 이름을 IT 변경
+		
+		// delete
+		// 20241002 학생 삭제
+		
+	}
+}
