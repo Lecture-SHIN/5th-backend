26bf3af423f27f5efe8e4ff400d7890db618c3fa
Lecture-SHIN <lecture.yonggishin@gmail.com>
2026-02-27 11:46:42 +0900

    update step03-thymeleaf

--- step03-thymeleaf/src/main/java/com/spring/mvc/controller/FormController.java
+++ step03-thymeleaf/src/main/java/com/spring/mvc/controller/FormController.java
@@ -3,12 +3,23 @@ package com.spring.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.spring.mvc.dto.Student;
 import com.spring.mvc.dto.Student.Grade;
 
 @Controller
 public class FormController {
+	
+	@GetMapping(value = "/input-form")
+	public String moveInputForm() {
+		return "input";
+	}
+	
+	@PostMapping(value = "/input-data")
+	public String processInputData() {
+		return "output";
+	}
 	
 	@GetMapping(value = "/form")
 	public String form(Student student,
