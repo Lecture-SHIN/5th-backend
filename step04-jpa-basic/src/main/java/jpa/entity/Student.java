3092deb03dfc9b8b99c6d0eea37dd2e11bcb6ca4
Lecture-SHIN <lecture.yonggishin@gmail.com>
2026-02-27 15:23:29 +0900

    add step04-jpa-basic

--- step04-jpa-basic/src/main/java/jpa/entity/Student.java
+++ step04-jpa-basic/src/main/java/jpa/entity/Student.java
@@ -1,5 +1,8 @@
 package jpa.entity;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -16,6 +19,15 @@ import lombok.ToString;
 @Entity
 public class Student {
 	@Id
+	@Column(name = "sid")
 	private Integer sid;
+
+	@Column(name="sname")
 	private String sname;
+//	@Column(columnDefinition = "VARCHAR(30) DEFAULT 'SNAME'", insertable = false, updatable = false)
+//	@Column(name = "sname", nullable = false, length = 10)
+	
+//	@Column(precision = 10, scale = 2)
+//	private BigDecimal price;
+	
 }
